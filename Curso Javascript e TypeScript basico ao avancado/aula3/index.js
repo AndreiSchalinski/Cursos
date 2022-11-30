4c8acb55fb7eccb5ad2db2c14ed12ae3556b9a15
AndreiSchalinski <andreischalinski@hotmail.com>
2022-11-30 08:51:07 -0300

    Neste curso, estudo Javascript, TypeScript básico ao avançado e vejo também, Node, React, NextJS, Mysql.

---
+++ Curso Javascript e TypeScript basico ao avancado/aula3/index.js
@@ -0,0 +1,7 @@
+console.log('Teste andrei');
+console.log("Andrei Schalinski");
+console.log(`"Andrei" com 'aspas'`); //textos dentro de aspas ou crase chama-se String
+
+console.log(234,`\n`,234);
+
+console.log(`Meu nome é "Andrei Schalinski". Estou aprendendo 'JavaScript' às`,10,`da manhã.`);
---
+++ Curso Javascript e TypeScript basico ao avancado/aula4/comentarios.js
@@ -0,0 +1,13 @@
+//Olá pessoal, tudo bem?
+
+console.log(`Aqui funciona`); //Esse também não será apresentado.
+console.log(`Linha 1`);
+console.log(`Linha 2`);
+console.log(`Linha 3`);
+console.log(`Linha 4`);
+//Será ignorado também.
+
+/*
+    Esse blovo de código não será lido
+    nem interprado pelo motor do chrome.
+*/
