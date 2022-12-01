7dee8f6e72fb3394c3f7b8a1e3a9855e8f2d1730
AndreiSchalinski <andreischalinski@hotmail.com>
2022-12-01 12:24:11 -0300

    Neste curso, estudo Javascript, TypeScript básico ao avançado e vejo também, Node, React, NextJS, Mysql.

---
+++ Curso Javascript e TypeScript basico ao avancado/aula15/index.js
@@ -0,0 +1,40 @@
+//Mais operações com Strings
+
+//Strings são indexadas
+let texto = 'Bom dia';
+console.log(`${texto[2]}`);
+
+let retorno = texto.charAt(4);
+console.log(`${retorno}`);
+
+//concatenações
+let nome = 'Andrei';
+let sobrenome = ' Schalinski';
+let nomeCompleto = nome.concat(sobrenome);
+console.log(`${nomeCompleto}`);
+console.log(`${nome+sobrenome}`);
+
+//índice em que inicia uma determinada palavra pode ser encontrado usando a função indexOf
+let frase = 'Andei de bike hoje.';
+let objeto = frase.indexOf('bike');
+console.log(`${objeto}`);
+objeto = frase.indexOf('bike', 2);
+console.log(`${objeto}`);
+
+//Como a indexOf também tem a lastIndexOf para encontrar de trás para frente.
+objeto = frase.lastIndexOf('bike',19);
+console.log(`${objeto}`);
+
+//Match é uma função para encontrar caractéres através de regex
+let segundoTexto = 'Moro na rua Alagoas, n 193.';
+let resultado = segundoTexto.match(/[0-9]+/)
+console.log(`${resultado}`);
+
+//Replace é uma função para substituir um caractere ou frase por outra.
+resultado = segundoTexto.replace('Alagoas', 'Caxias');
+console.log(`${resultado}`);
+
+//length é uma função para ver tamanho de string
+console.log(`${resultado.length}`);
+
+//slice é função para coletar fragmento de string, do início ao fim
