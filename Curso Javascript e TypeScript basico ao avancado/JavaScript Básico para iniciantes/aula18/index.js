7ac72de44913e7edd0158da17e43d4f1889df544
AndreiSchalinski <andreischalinski@hotmail.com>
2022-12-01 19:27:13 -0300

    Neste curso, estudo Javascript, TypeScript básico ao avançado e vejo também, Node, React, NextJS, Mysql.

---
+++ Curso Javascript e TypeScript basico ao avancado/JavaScript Básico para iniciantes/aula18/index.js
@@ -0,0 +1,11 @@
+//Classe Math para trabalhar com matemática
+
+let numero = 10.234525;
+
+console.log(Math.floor(numero));//função que arredonda pra baixo
+console.log(Math.ceil(numero));//função que arredonda pra cima
+console.log(Math.round(numero));//função que arredonda se baseando no valor, para ser pra cima ou baixo
+console.log(Math.min(12,4,7,1,23,9,32));//função que pega o menor valor da lista
+console.log(Math.max(12,4,7,1,23,9,32));//função que pega o maior valor da lista
+console.log(Math.PI);//função que pega o valor pi
+console.log(Math.pow(2,3));//função que trabalha com potenciação
---
+++ Curso Javascript e TypeScript basico ao avancado/JavaScript Básico para iniciantes/aula19/index.js
@@ -0,0 +1,14 @@
+//Arrays
+
+const lista = ['Luiz','Karla',23,'Bianca','Roberto',45,333, true];
+lista.unshift('Fusca')//Função adiciona elemento na primeiro posição da array
+lista.push('Ferrari');//Função adiciona elemento na array
+lista.pop()//Remove último elemento da array;
+lista.shift()//Remove primeiro elemento da array
+
+console.log(lista);
+console.log(lista instanceof Array);//para saber se é uma array que está sendo trabalhada.
+
+
+
+
